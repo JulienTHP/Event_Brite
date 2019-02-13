f399c070f87379dd56a1bfb635221d105b9515fd
Thomas CLAUDEL <zekidok@gmail.fr>
2019-02-13 23:11:46 +0100

    payement ok

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,50 +1,57 @@
 class AttendancesController < ApplicationController
-	#before_action :set_event, only: [:new, :create, :index]
-	#before_action :is_not_your_event, only: [:index]
+	before_action :set_event, only: [:new, :create, :index]
+	before_action :is_not_your_event, only: [:index]
 
-
-	def index
-	end
-
-	def new
+	def set_event
 		@event = Event.find(params[:event_id])
-		attendance = Attendance.new
-		@userattendance = @event.attendances.map { |attendance| attendance.user  }
+  	@amount = @event.price * 100
 	end
 
-	def create
-  # Amount in cents
-  @amount = @event.price * 100
-
-  customer = Stripe::Customer.create(
-  	:email => params[:stripeEmail],
-  	:source  => params[:stripeToken]
-  	)
-
-  charge = Stripe::Charge.create(
-  	:customer    => customer.id,
-  	:amount      => @amount,
-  	:description => 'Rails Stripe customer',
-  	:currency    => 'usd'
-  	)
-
-  @attendance = Attendance.new(user: current_user, event:@event)
-  @attendance.stripe_customer_id = "l'évènement est gratuit"
-
-  if @attendance.save!
-  	flash[:succes] = "Vous avez bien réserver une place pour cet évènement"
-
-  else 
-  	flash[:error] = "Erreur lors de la réservation"
-  	p @attendance.errors
-  	render :new
+  def is_not_your_event
+    unless @event.admin == current_user
+      flash[:danger] = 'Back off ! Not your event !'
+      redirect_to root_path
+    end
   end
 
-	rescue Stripe::CardError => e
-	flash[:error] = e.message
-	redirect_to new_charge_path
+  def index
+  end
 
-	end
+  def new
+  	p '33333333333333'
+  	p params
+  	@attendance = Attendance.new
+  	@usersattandances = @event.attendances.map {|attendance| attendance.user }
+  end
+
+  def create
+  	
+	  	customer = Stripe::Customer.create(
+		    :email => params[:stripeEmail],
+		    :source  => params[:stripeToken]
+		  )
+
+		  charge = Stripe::Charge.create(
+		    :customer    => customer.id,
+		    :amount      => @amount,
+		    :description => 'Rails Stripe customer',
+		    :currency    => 'eur'
+		  )
+		@attendance = Attendance.new(user: current_user, event: @event)
+		@attendance.stripe_customer_id = charge.customer
+
+		if @attendance.save!
+			flash[:success] = 'Reservation reussie !'
+			# redirect_to event_path(@event.id)
+		else
+			flash[:error] = 'Erreur lors de la reservation'
+			p @attendance.errors
+			render :new
+		end
+		rescue Stripe::CardError => e
+		  flash[:error] = e.message
+		  redirect_to new_charge_path
+end
 end
 
 
